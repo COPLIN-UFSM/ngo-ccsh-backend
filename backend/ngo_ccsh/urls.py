93ab372de42fd6f7f028b36665f9268c9eba28fe
henryzord <henry.cagnini@gmail.com>
2026-05-20 12:55:27 -0300

    fix to serializer with incorrect fields

--- backend/ngo_ccsh/urls.py
+++ backend/ngo_ccsh/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularRedocView, SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
