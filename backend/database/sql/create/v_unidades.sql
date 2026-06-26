b0bc43a777ad117d43c2dd2500acc0814d45f22a
henryzord <henry.cagnini@gmail.com>
2026-06-26 18:27:36 -0300

    minor fix to view creation sql

--- backend/database/sql/create/v_unidades.sql
+++ backend/database/sql/create/v_unidades.sql
@@ -1,6 +1,6 @@
-create view V_UNIDADES as (
+create view orcamento.V_UNIDADES as (
     select ID_UNIDADE, STRIP(NOME_UNIDADE) NOME_UNIDADE, COD_ESTRUTURADO, ID_CENTRO, TIPO_UNIDADE_ITEM ID_TIPO_UNIDADE, SITUACAO_ITEM ID_SITUACAO_UNIDADE
     from bee.NAV_UNIDADES
-    where SITUACAO_ITEM not in (3, 4) -- extinta e desativada
+    where ID_CENTRO not in (121, 2021) -- poli e ctism (antigos)
 );
-comment on table V_UNIDADES is 'Criada a partir de bee.nav_unidades.';
+comment on table orcamento.V_UNIDADES is 'Criada a partir de bee.nav_unidades.';
