713ffb5ee23a5163159cb0d4073fa1437865bc3c
Leandro Galbarino <leandro.galbarino@pitang.com>
2026-07-09 22:22:06 -0300

    feat: criando as views das entidades.

--- backend/entidades/views.py
+++ backend/entidades/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.exceptions import ValidationError, MethodNotAllowed
 from rest_framework.response import Response
 
@@ -24,7 +23,7 @@ from entidades.serializers import (
     PessoaSerializer, UnidadeSerializer
 )
 
-
+detailNotAllowed = "Método não permitido para unidades cadastrados no sie"
 class CentroViewSet(viewsets.ModelViewSet):
     queryset = Centro.objects.all()
     serializer_class = CentroSerializer
@@ -32,13 +31,13 @@ class CentroViewSet(viewsets.ModelViewSet):
     # OK ?
     # TODO depende se pode mexer! se tiver centro_sie, não pode mexer. do contrário, pode
 
-    def partial_update(self,req, *args, **kwargs):
-        centro = self.get_object()
+    def partial_update(self,request, *args, **kwargs):
+        centro: Centro = self.get_object()
 
         if centro.centro_sie is not None:
-            raise MethodNotAllowed(detail="Método não permitido para centros cadastrados no sie", method="PATCH")
+            raise MethodNotAllowed(detail=detailNotAllowed, method="PATCH")
 
-        serializer = self.get_serializer(centro, data=req.data, partial=True)
+        serializer = self.get_serializer(centro, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
 
         serializer.save()
@@ -56,13 +55,13 @@ class UnidadeViewSet(viewsets.ModelViewSet):
     # TODO mostrar centro
     # TODO mostrar situacaoUnidade
     # TODO mostrar tipoUnidade
-    def partial_update(self,req, *args, **kwargs):
-        unidade = self.get_object()
+    def partial_update(self,request, *args, **kwargs):
+        unidade: Unidade = self.get_object()
 
-        if unidade.centro_sie is not None:
-            raise MethodNotAllowed(detail="Método não permitido para unidades cadastrados no sie", method="PATCH")
+        if unidade.unidade_sie is not None:
+            raise MethodNotAllowed(detail=detailNotAllowed, method="PATCH")
 
-        serializer = self.get_serializer(unidade, data=req.data, partial=True)
+        serializer = self.get_serializer(unidade, data=request.data, partial=True)
         serializer.is_valid(raise_exception=True)
 
         serializer.save()
@@ -73,29 +72,50 @@ class UnidadeViewSet(viewsets.ModelViewSet):
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
-    http_method_names = ["get"]
+    http_method_names = ["get", "post", "patch"]
     # !OK
+    def partial_update(self, request, *args, **kwargs):
+        curso = self.get_object()
+        if curso.centro_sie is not None:
+            return MethodNotAllowed(detail=detailNotAllowed, method="PATCH")
+        serializer = self.get_serializer(curso, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
 
+        serializer.save()
+        return Response(serializer.data)
 
 class PessoaViewSet(viewsets.ModelViewSet):
-    queryset = Pessoa.objects.all()
+    queryset = Pessoa.objects.all().select_related('email', 'telefone')
     serializer_class = PessoaSerializer
+    http_method_names = ["get", "post", "patch"]
+
+    def partial_update(self, request, *args, **kwargs):
+        pessoa = self.get_object()
+        if pessoa.pessoa_sie is not None:
+            return MethodNotAllowed(detail=detailNotAllowed, method="PATCH")
+        serializer = PessoaSerializer(instance=pessoa, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
     # TODO depende se pode mexer! se tiver id_pessoa_sie, não pode mexer. do contrário, pode
     # TODO mostrar e-mail e telefone
+
 
 class DiscenteViewSet(viewsets.ModelViewSet):
     queryset = Discente.objects.all().select_related('email', 'curso')
     serializer_class = DiscenteSerializer
     http_method_names = ["get"]
-    # !OK
+
     # TODO mostrar e-mail e telefone
     # TODO mostrar curso
+
 
 
 class ServidorViewSet(viewsets.ModelViewSet):
     queryset = Servidor.objects.all().select_related('email', 'curso', 'cargo')
     serializer_class = ServidorSerializer
     http_method_names = ["get"]
-    # !OK
+
     # TODO mostrar e-mail e telefone
     # TODO mostrar cargo
